2472d5fe40ff01df38e01fd29b7d4db7faac0765
bhupendragahlot <gahlotbittu11@gmail.com>
2025-03-20 15:28:35 +0530

    Update vite.config.js
    update

--- kovij-fitness-zone/vite.config.js
+++ kovij-fitness-zone/vite.config.js
@@ -5,9 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 3000, // specify the port you need
-  },
+  allowedHosts: ['kovij.onrender.com']
+},
   plugins: [react(),
     tailwindcss(),
   ],
