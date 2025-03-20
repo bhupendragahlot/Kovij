4f99719ee6826a52a1825c263455a71325047407
bhupendragahlot <gahlotbittu11@gmail.com>
2025-03-20 18:02:08 +0530

    update

--- kovij-fitness-zone/src/components/Contact.jsx
+++ kovij-fitness-zone/src/components/Contact.jsx
@@ -28,13 +28,14 @@ function Contact() {
 
     try {
       // Use the Vercel API route
-      const response = await fetch("http://localhost:5000/api/send-email", {
+      const response = await fetch("/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       })
+console.log(formData);
 
       const data = await response.json()
 
