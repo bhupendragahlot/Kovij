1db288950dbd9c930c0c3644a586b61f3599f623
bhupendraTestGit <bhupendra.test11@gmail.com>
2025-06-21 23:55:24 +0530

    Update server.js
    remove {
      useNewUrlParser: true,
      useUnifiedTopology: true,
    }

--- server/server.js
+++ server/server.js
@@ -27,9 +27,7 @@ app.use(cookieParser());
 app.use(cookieMiddleware);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_URI
 }).then(() => console.log("MongoDB connected")).catch(error => console.error("MongoDB connection error:", error));
 
 
