302dae3a511db2ea10ddd3cd01b22b4df8492fe6
bhupendraTestGit <bhupendra.test11@gmail.com>
2025-06-21 23:51:58 +0530

    Update server.js
    add process.env.MONGO_URI  ,

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(cookieMiddleware);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI{
+mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected")).catch(error => console.error("MongoDB connection error:", error));
